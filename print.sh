4eb7f5f531ee84586f1369c7ff49700970118d27
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-09-29 16:12:22 +0530

    test4

--- print.sh
+++ print.sh
@@ -7,3 +7,16 @@ echo Hello world
 # BLUE    -34
 # MAGENTA -35
 # CYAN    -36
+
+# Syntax of Color print
+# echo -e "\e[COLmMESSAGE\[0m"
+# -e option to enable color
+# \e[COLM - To defined which color
+# \e[0m -  To disable color
+
+echo -e "\e[31mRED COLOR\e[0m"
+echo -e "\e[32mGREEN COLOR\e[0m"
+echo -e "\e[33mYELLOW COLOR\e[0m"
+echo -e "\e[34mBLUE COLOR\e[0m"
+echo -e "\e[35mMAGENTA COLOR\e[0m"
+echo -e "\e[36mCYAN COLOR\e[0m"
