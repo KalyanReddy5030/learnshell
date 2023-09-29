a314be58ef546dbad5336991e32947e8d71e24ef
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-09-29 13:32:40 +0530

    test4

---
+++ 04-functions.sh
@@ -0,0 +1,10 @@
+# if we give name(abc) to commands(cmd 1,2,3) then it is called a function abc().
+# The commands which were having inside the {..} was commands
+
+#How to declare function.
+
+abc() {
+  echo hello            # command 1
+  echo hello world      # command 2
+  echo hello universe   # command 3
+}
