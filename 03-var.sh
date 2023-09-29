69acbf8d3047ec9972f55ea1acc5c0089bfaa335
Kalyan Reddy <kalyanreddy5030@gmail.com>
2023-09-29 10:22:49 +0530

    test4

---
+++ 03-var.sh
@@ -0,0 +1,11 @@
+# if we give particular name to your data is called variable.
+#syntax
+#var_name = data
+a= 10
+name=devops
+b=20
+
+#how to access variable
+
+echo value of a =$a
+echo value of name = $name
